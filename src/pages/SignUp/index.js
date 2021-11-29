5afb8cd5e588727a39a8f4ea29e73824402b8a76
Arthur Teixeira <52799942+arthurtc30@users.noreply.github.com>
2021-11-29 14:49:17 -0300

    Tela cadastro

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,11 +1,36 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import logo from "../../assets/logo.png";
 
 function SignUp() {
-    return (
-      <div>
-        <h1>PÁGINA DE CADASTRO</h1>
-      </div>
-    );
+  const [nome, setNome] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  function handleSubmit(e) {
+    e.preventDefault();
   }
-  
-  export default SignUp;
-  
+
+  return (
+    <div className="container-center">
+      <div className="login">
+        <div className="logo-area">
+          <img src={logo} alt="Sistema Logo" />
+        </div>
+
+        <form onSubmit={handleSubmit}>
+          <h1>Criar uma conta</h1>
+          <input type="text" placeholder="Seu nome" value={nome} onChange={(e) => setNome(e.target.value)} />
+          <input type="text" placeholder="email@email.com" value={email} onChange={(e) => setEmail(e.target.value)} />
+          <input type="password" placeholder="******" value={password} onChange={(e) => setPassword(e.target.value)} />
+          <button type="submit">Cadastrar</button>
+        </form>
+
+        <Link to="/">Já tem uma conta?</Link>
+      </div>
+    </div>
+  );
+}
+
+export default SignUp;
+
