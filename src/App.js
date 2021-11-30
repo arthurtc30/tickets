b5052cbf933eba9b2391a45244cad211dc6679da
Arthur Teixeira <52799942+arthurtc30@users.noreply.github.com>
2021-11-30 11:01:59 -0300

    Adding toasts
    added react-toastify

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import { BrowserRouter } from 'react-router-dom';
 import AuthProvider from './contexts/auth';
-import RouteWrapper from './routes'
+import RouteWrapper from './routes';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
     <AuthProvider>
       <BrowserRouter>
+      <ToastContainer autoClose={2000} />
         <RouteWrapper />
       </BrowserRouter>
     </AuthProvider>
