a0f6ca03f57d1245a28fab52eef57e95f5c69f57
Arthur Teixeira <52799942+arthurtc30@users.noreply.github.com>
2021-11-30 08:36:16 -0300

    Criando contexto

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import { BrowserRouter } from 'react-router-dom';
+import AuthProvider from './contexts/auth';
 import Rotas from './routes'
 
 function App() {
   return (
-    <BrowserRouter>
-      <Rotas />
-    </BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter>
+        <Rotas />
+      </BrowserRouter>
+    </AuthProvider>
   );
 }
 
